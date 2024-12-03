c38d4ef736c98077b9e990e5735ace8eab6ce5bf
wb <wubing7755@gmail.com>
2024-12-03 23:00:24 +0800

    new feature

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,62 +1,14 @@
-using Microsoft.EntityFrameworkCore;
-using Server.Database;
-using Server.Service;
+namespace Server;
 
-namespace Server
+public static class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-            builder.Services.AddControllers();
+        app.MapGet("/", () => "Hello World!");
 
-            // Configure service
-            builder.Services.ConfigureServices(builder.Configuration);
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-            app.UseAuthorization();
-
-            app.MapControllers(); // Simplified controller route mapping
-            app.MapRazorPages();
-
-            // Apply database migrations
-            using (var scope = app.Services.CreateScope())
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
-                dbContext.Database.Migrate(); // Apply all pending migrations
-            }
-
-            app.Run();
-        }
-    }
-
-    public static class ServiceConfigurationExtensions
-    {
-        public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
-        {
-            //services.AddDbContext<DatabaseContext>(options =>
-            //    options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddEntityFrameworkSqlite().AddDbContext<DatabaseContext>();
-
-            services.AddScoped<DbContextService>();
-        }
+        app.Run();
     }
 }
